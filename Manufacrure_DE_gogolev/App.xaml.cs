1fc2cb94dbe70070b77637f41b2c5af4eadacf38
user9 <user9@WIN-0JD1SONFABF>
2025-11-19 15:01:40 +0300

    Добавьте файлы проекта.

---
+++ Manufacrure_DE_gogolev/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Manufacrure_DE_gogolev.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Manufacrure_DE_gogolev
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static user9Entities context= new user9Entities();
+        public static SystemUser CurrentUser { get; set; }
+    }
+}
---
+++ Manufacrure_DE_gogolev/View/Windows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Manufacrure_DE_gogolev.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTB.Text) || string.IsNullOrEmpty(PasswordPB.Password))
+            {
+                MessageBox.Show("Заполните все поля!","Предупреждение",MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                App.CurrentUser = App.context.SystemUser.FirstOrDefault(systemUser => systemUser.Login == LoginTB.Text && systemUser.Password == PasswordPB.Password);
+
+                if(App.CurrentUser !=null)
+                {
+                    CaptchaWindow captchaWindow = new CaptchaWindow();
+                    if (captchaWindow.ShowDialog() == true)
+                    {
+                        //Аутентификация
+                    }
+                    else
+                    {
+                        //Блокировка
+                    }
+                }
+                else
+                {
+                    //Блокировка
+                }
+            }
+
+        }
+
+        private void LoginBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
